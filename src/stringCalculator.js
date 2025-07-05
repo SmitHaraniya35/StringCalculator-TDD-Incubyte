60bacc0ccf655ed96b33589b9a7f65224f6def00
SmitHaraniya35 <smeetharaniya@gmail.com>
2025-07-05 16:09:39 +0530

    Feat: return sum of numbers separated by single custom delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,11 +1,19 @@
-function add(input){
+function add(input) {
     // Check if input is empty string
-    if(input === "") 
+    if (input === "")
         return 0;
 
-    // Handling default delimiters and split input on it
-    const defaultDelimiters = /[\n,]/;
-    return input.split(defaultDelimiters).map(Number).reduce((a, b) => a + b, 0);
+    // Assign default delimiters 
+    let delimiter = /[\n,]/;
+
+    if (input.startsWith("//")) {
+        const [delimiterPart, numbersPart] = input.split('\n');
+        const customDelimiter = delimiterPart.slice(2);
+        delimiter = new RegExp(customDelimiter);
+        input = numbersPart;
+    }
+
+    return input.split(delimiter).map(Number).reduce((a, b) => a + b, 0);
 
 }
 
