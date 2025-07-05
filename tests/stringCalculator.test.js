3c61b527fb235ac20b10db20dcdd2d27751c659c
SmitHaraniya35 <smeetharaniya@gmail.com>
2025-07-05 15:18:24 +0530

    Test: return the number itself for single number

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -8,4 +8,8 @@ describe("Test String Calculator", () => {
     test('It should return 0 for empty string', () => {
         expect(add("")).toBe(0);
     });
+
+    test('It should return the number itself for single number', () => {
+        expect(add("1")).toBe(1);
+    });
 })
